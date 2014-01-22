68327d83c232d8ba8c720d5c60589f5802d25b8b
Shashy Dass <ShashyD@gmail.com>
2014-01-22 05:52:53 -0500

    handler for website, dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.4384"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
+                 [com.datomic/datomic-free "0.9.4384"]
+                 [compojure "1.1.6"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [hiccup "1.0.4"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler schemad.handler/app}
+  :profiles
+    {:dev {:dependencies [[midje "1.6.0"]
+                          [javax.servlet/servlet-api "2.5"]]}})
---
+++ src/schemad/handler.clj
@@ -0,0 +1,4 @@
+(ns schemad.handler
+  (:require [compojure.core :refer (defroutes GET POST)]
+            [schemad.core :as core]
+    ))
